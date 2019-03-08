3ace775bbddb91bf275d35ba01c1d73ee9b32408
EC2 Default User <ec2-user@ip-172-31-29-71.us-east-2.compute.internal>
2019-03-08 23:28:54 +0000

    Added uwsgi

--- veryGoodSite/veryGoodSite/settings.py
+++ veryGoodSite/veryGoodSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1&ffsh2*sb6*r_uih1&!1juh%y_7xf+e)%2b9--4!*-41&*&gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['ec2-18-219-152-5.us-east-2.compute.amazonaws.com','0.0.0.0']
 
 
 # Application definition
