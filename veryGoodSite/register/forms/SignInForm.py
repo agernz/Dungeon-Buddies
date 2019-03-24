5dc084682392cd0758731d51af988397633f9daa
agernz <agernz@gmail.com>
2019-03-24 00:18:37 -0500

    seperated forms to seperate files

--- veryGoodSite/register/forms/SignInForm.py
+++ veryGoodSite/register/forms/SignInForm.py
@@ -6,11 +6,8 @@ onlyCharNum = RegexValidator(r'^[a-zA-Z0-9]+$',
                              "Field can only contain letters and numbers.")
 
 
-class RegisterForm(forms.Form):
+class SignInForm(forms.Form):
     username = forms.CharField(label="Username",
                                max_length=30,
                                validators=[onlyCharNum])
-    characterName = forms.CharField(label="Character Name",
-                                    max_length=30,
-                                    validators=[onlyCharNum])
     password = forms.CharField(min_length=8, max_length=120)
