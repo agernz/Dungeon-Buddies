95eebc2780ccfa4dc03d7583d24d44d34b6150ce
Adam Dama <dama2@illinois.edu>
2019-03-26 15:02:08 -0500

    play page ui

--- veryGoodSite/game/views.py
+++ veryGoodSite/game/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'game/index.html')
 
-
 @login_required
 def guild(request):
     guilds = ['abc', '123']
