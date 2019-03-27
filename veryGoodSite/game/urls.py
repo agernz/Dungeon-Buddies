9b014b236545761d0bee92c957fed1364d4dc988
agernz <agernz@gmail.com>
2019-03-27 16:44:58 -0500

    added creat guild url

--- veryGoodSite/game/urls.py
+++ veryGoodSite/game/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='game-index'),
     path('guild/', views.guild, name='game-guild'),
+    path('guild-create/', views.createGuild, name='game-guild-create'),
 ]
