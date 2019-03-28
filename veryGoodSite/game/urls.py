3ffcb9a62bf65d991c2f40ff9340304d924e46e0
agernz <agernz@gmail.com>
2019-03-27 23:14:36 -0500

    routing for joining a user

--- veryGoodSite/game/urls.py
+++ veryGoodSite/game/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='game-index'),
-    path('guild/', views.guild, name='game-guild'),
+    path('guild/', views.guildPage, name='game-guild'),
     path('guild-create/', views.createGuild, name='game-guild-create'),
+    path('guild-join/', views.joinGuild, name='game-guild-join'),
 ]
