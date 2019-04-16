e29300eca749ebc85e0456f4ac930eff99f8331e
agernz <agernz@gmail.com>
2019-04-16 09:33:58 +0000

    thins are working as of this moment in time and space. Please work

--- veryGoodSite/veryGoodSite/urls.py
+++ veryGoodSite/veryGoodSite/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/staticfiles/favicon.ico')),
     path('', include('register.urls')),
     path('', include('game.urls'))
 ]
